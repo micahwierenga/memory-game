a3e4d808986d468b562e5de5b236b409ca27abd1
micahwierenga <wierenga.micah@gmail.com>
2020-05-07 08:17:23 -0600

    Initial commit

---
+++ starter-code/js/main.js
@@ -0,0 +1,42 @@
+/*------Constants------*/
+
+const difficulty = {
+    "Easy": 5,
+    "Medium": 10,
+    "Hard": 15
+}
+
+/*------Variables------*/
+
+let winTime, seconds, matchCount, card1Val, card1Idx, card2Val, card2Idx, turn, timer, deck;
+let cardsInPlay = [];
+
+/*------Cached Element References------*/
+
+const resetDiv = document.getElementById('resetDiv');
+const difficultyButtonsDiv = document.getElementById('difficultyButtons');
+const messageEl = document.getElementById('message');
+const playArea = document.getElementById('playArea');
+const resetBtn = document.getElementById('resetButton');
+
+/*------Event Listeners------*/
+
+playArea.addEventListener('click', function() {
+
+})
+
+resetBtn.addEventListener('click', function() {
+    buttonDiv.setAttribute("class", "");
+    resetDiv.setAttribute("class", "hidden");
+})
+
+difficultyButtonsDiv.addEventListener('click', function(evt) {
+    if (evt.target.innerText) {
+        buttonDiv.setAttribute("class", "hidden");
+        resetDiv.setAttribute("class", "");
+    }
+})
+
+
+/*------Functions------*/
+
